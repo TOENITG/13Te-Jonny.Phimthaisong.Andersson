8d0bc671a8b768911e36875a65e4189bba656ee9
Jonny <japhim@gmail.com>
2014-10-09 15:31:36 +0200

    Java
    Just some comments

---
+++ NameDateAdress/src/namedateadress/NameDateAdress.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package namedateadress;
+
+/**
+ *
+ * @author jonny.phimthaisongan
+ */
+public class NameDateAdress {
+import javax.swing.JOptionPane;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /**
+         * Namn
+         * Person Number
+         * Adress
+         * Vissa bara födelsedagen och inte 4 sista siffror
+         * Välkommen <Namn>, då du fyller den <Del av personnummer som visar födelsedagen> kommer vi komma till dig, <Address> och fira dig. Detta är ett automatiskt meddelande.
+         */
+        
+    }
+    
+}
