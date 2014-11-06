218605779e765bd4ae041fbf1ce66b636c4c6aaf
Jonny <japhim@gmail.com>
2014-11-06 16:29:12 +0100

    Upggift 2?
    Jag har gjort upgift 2? eller 3? what ever. Upgiften är den som heter
    Plus

---
+++ Classes/src/classes/Classes.java
@@ -0,0 +1,21 @@
+
+package classes;
+
+
+public class Classes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    float first_number, second_number, answer;
+    
+    first_number = 10.1f;
+    second_number = 2.8f;
+    answer = first_number + second_number;
+    System.out.println("Total:" + answer);
+    }
+    
+}
---
+++ Plus/src/plus/Plus.java
@@ -0,0 +1,54 @@
+
+package plus;
+import java.util.*;
+
+public class Plus {
+    
+    public static void main(String[] args) {
+
+
+        System.out.println("Number:");
+        double number = input();
+
+
+        System.out.println("Next Number:");
+        number = number + input();
+
+        System.out.println("Addition:" + number);
+
+        System.out.println("Next Number:");
+        number = number - input();
+
+        System.out.println("Subtraktion:" + number);
+
+        System.out.println("Next Number:");
+        number = number * input();
+
+        System.out.println("Multiplikation:" + number);
+
+        System.out.println("Next Number:");
+        number = number / input();
+
+        System.out.println("Division:" + number);
+
+    }
+    
+    static double input() {
+        double number = 0;
+        Scanner userinput = new Scanner(System.in);
+        boolean error;
+        do {
+
+            try{
+                number = Double.parseDouble(userinput.nextLine());
+                error = true;
+            }
+            catch (NumberFormatException e){
+                System.out.println("That was not a number");
+                error = false;
+            }
+        }
+        while (error == false);
+        return number;
+    }
+}
